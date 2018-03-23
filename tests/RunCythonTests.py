b89e383ecfff20041dc633209aa2335babfce45f
FlorianSauer <sauer.florian@gmx.de>
2018-03-23 06:02:54 +0100

    improving verbosity

--- tests/RunCythonTests.py
+++ tests/RunCythonTests.py
@@ -14,6 +14,16 @@ def consumeToken(bucket, name, iterations):
             print "reduced bucket from thread " + name
 
 
+def debugprint(size, refilltime, refillamount, testamount, parallelthreads):
+    for _ in xrange(testamount * parallelthreads):
+        print "should consume", size, "tokens"
+        print "should wait", refilltime, "seconds"
+        for i in xrange(refilltime):
+            print "waiting..."
+            time.sleep(1)
+        print "should refill", refillamount, "tokens"
+
+
 if __name__ == "__main__":
 
     iterations = 3
@@ -32,20 +42,24 @@ if __name__ == "__main__":
 
     print "this test will consume a total amount of", (iterations * parallelthreads), "tokens"
     print "this test should take at least", (((iterations * parallelthreads) / refillamount) * refillrate) - refillrate, "seconds"
-
-    past = time.time()
-
     print
     print "starting test"
     print
+
+    past = time.time()
 
     for i in xrange(parallelthreads):
         t = threading.Thread(target=consumeToken, args=(bucket, str(i), iterations))
         threads.append(t)
 
+    debugthread = threading.Thread(target=debugprint,
+                                   args=(size, refillrate, refillamount, iterations, parallelthreads))
+
+    debugthread.start()
     for t in threads:
         t.start()
 
+    debugthread.join()
     for t in threads:
         t.join()
 

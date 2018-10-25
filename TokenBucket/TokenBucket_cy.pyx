83b15f451cbea26aeadcc77459bee4fe3c0538b1
Florian Sauer <FlorianSauer@users.noreply.github.com>
2018-10-25 10:37:49 +0200

    improving cython types, fixing bugs

--- TokenBucket/TokenBucket_cy.pyx
+++ TokenBucket/TokenBucket_cy.pyx
@@ -1,5 +1,6 @@
 import threading
 import time
+from libc cimport math
 
 from TokenBucketErrors import TimeoutError, TokenAmountError, BucketSizeError
 
@@ -11,14 +12,14 @@ cdef class TokenBucket(object):
     Cython implementation
     """
 
-    cdef public int size
-    cdef public int value
-    cdef public float refill_rate
-    cdef public int refill_amount
+    cdef public long size
+    cdef public long value
+    cdef public double refill_rate
+    cdef public long refill_amount
     cdef public object mutex
-    cdef public int last_update
+    cdef public double last_update
 
-    def __init__(self, size, refillrate, refillamount, value=None, lastupdate=None):
+    def __init__(self, long size, double refillrate, long refillamount, long value=-1, long lastupdate=-1):
         # type: (int, float, int, int, int) -> None
         """
         :param size: The size of the Token Bucket
@@ -34,12 +35,12 @@ cdef class TokenBucket(object):
         self.refill_amount = refillamount
         self.mutex = threading.RLock()
         self.last_update = time.time()
-        if value:
+        if value != -1:
             self.value = value
-        if lastupdate:
+        if lastupdate != -1:
             self.last_update = lastupdate
 
-    cdef int _refill_count(self, now=None):
+    cdef long _refill_count(self, double now=-1):
         # type: (float) -> int
         """
         calculate the amount of refill operations that can be executed,
@@ -65,11 +66,11 @@ cdef class TokenBucket(object):
         # refilltime is 10
         #            (    15     -       7         ) /       10           = 8 / 10 = 0.8 = int 0
         with self.mutex:
-            if not now:
+            if now == -1:
                 now = time.time()
-            return int(((now - self.last_update) / self.refill_rate))
+            return <long> ((now - self.last_update) / self.refill_rate)
 
-    cpdef int current_filllevel(self):
+    cpdef long current_filllevel(self):
         # type: () -> int
         """
         returns the current fill level of the Token Bucket (self.value).
@@ -82,17 +83,19 @@ cdef class TokenBucket(object):
             self.refill()  # refilling inside mutex, because another thread could jump between refill() and return value
             return self.value
 
-    cpdef refill(self, now=None):
-        # type: (float) -> None
+    cpdef int refill(self, double now=-1, bint force=False) except -1:
+        # type: (float, bool) -> None
         """
         Performs a refill operation on the Token Bucket.
         The refill operation will only succeed, if the last update is long enough ago.
 
         :param now: Specifies the current time to use
+        :param force: perform a refill operation, even if no refill operation can be done at the moment.
         """
+        cdef long refill_count
 
         with self.mutex:
-            if not now:
+            if now == -1:
                 now = time.time()
             refill_count = self._refill_count(now)  # calculate how many refill operations we can perform
             if refill_count > 0:
@@ -101,11 +104,15 @@ cdef class TokenBucket(object):
                 # amount of refill operations * refilltime -> last update
                 # this is a logical calculation, based that time.time() is accurate
                 self.last_update += refill_count * self.refill_rate
+            elif force:
+                self.value += self.refill_amount
+                self.last_update += 1 * self.refill_rate
 
             if self.value > self.size:
                 self.value = self.size
+        return 0
 
-    cpdef bint consumeToken(self, int token_amount, bint blocking=True, timeout=None) except *:
+    cpdef bint consumeToken(self, long token_amount, bint blocking=True, double timeout=-1) except *:
         # type: (int, bool, float) -> bool
         """
         Consumes x tokens from the Token Bucket.
@@ -116,12 +123,14 @@ cdef class TokenBucket(object):
         :param timeout: specifies the maximum time to wait, if blocking is enabled
         :return: returns if token_amount tokens could be consumed or not
         :raises BucketSizeError: a BucketSizeError is raised, if the given token_amount is bigger than the maximum
-        bucket size
+                                 bucket size
         :raises TokenAmountError: a TokenAmountError is raised, if the current amount of tokens in the Bucket is 
-        smaller than token_amount and blocking is set to False
+                                  smaller than token_amount and blocking is set to False
         :raises TimeoutError: a TimeoutError is raised, if the bucket has not enough tokens and the time to wait for a 
-        sufficient amount of tokens woould take longer than the given timeout
+                              sufficient amount of tokens woould take longer than the given timeout
         """
+        cdef double now, time_to_wait
+        cdef long additional_tokes
 
         if token_amount > self.size:
             raise BucketSizeError("cannot consume more tokens than the maximum amount of tokens in the bucket")
@@ -148,11 +157,18 @@ cdef class TokenBucket(object):
                         time_to_wait = 0
 
                     # theoretical time to wait for additional tokens, substracted by timedelta
-                    time_to_wait = (additional_tokes * self.refill_rate) - time_to_wait
+                    if additional_tokes > self.refill_amount:
+                        # we want more tokens than a refill operation could provide, we have to wait longer than
+                        # one refill operation.
+                        time_to_wait = (math.ceil(<double> self.refill_amount/<double> additional_tokes) * self.refill_rate) - time_to_wait
+                    else:
+                        # we want less or equal tokens than a refill operation could provide, we only have to wait
+                        # for this refill operation to take place
+                        time_to_wait = self.refill_rate - time_to_wait
 
                     if time_to_wait < 0:
                         time_to_wait = 0
-                    if timeout:
+                    if timeout != -1:
                         if time_to_wait > timeout:
                             raise TimeoutError("not enough tokens aviable, waiting would be " + str(
                                 time_to_wait) + " seconds, but is longer than allowed timeout of " + str(
@@ -162,7 +178,7 @@ cdef class TokenBucket(object):
         # this is VERY ugly, exchange it with mutex.aquire()+mutex.release()
         # will change in the future, maybe
         time.sleep(time_to_wait)
-        if timeout:
+        if timeout != -1:
             timeout = timeout - time_to_wait
         return self.consumeToken(token_amount, blocking=blocking,
                                  timeout=timeout)  # lets try again to consume some tokens
